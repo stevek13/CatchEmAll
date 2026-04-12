f286d4a88eef698f692dbd2fe3a8cf7b0ca9abba
Steve Kite <skite1@hotmail.com>
2026-04-12 14:02:13 -0700

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Steve on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
